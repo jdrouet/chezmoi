3cc72c153bd204da785b03c894eb91691174b276
Jérémie Drouet <jeremie.drouet@gmail.com>
2024-11-26 14:11:03 +0100

    refactor: update miflora card
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- client/src/component/miflora_card.rs
+++ client/src/component/miflora_card.rs
@@ -3,56 +3,6 @@ use std::borrow::Cow;
 use another_html_builder::{Body, Buffer};
 use human_number::Formatter;
 
-fn render_empty_last_update<'v, W: std::fmt::Write>(
-    buf: Buffer<W, Body<'v>>,
-) -> Buffer<W, Body<'v>> {
-    buf.node("div")
-        .attr((
-            "class",
-            "card-content align-content-center text-center min-h-150px",
-        ))
-        .content(|buf| buf.text("No content found"))
-}
-
-fn render_last_update<'v, W: std::fmt::Write>(
-    buf: Buffer<W, Body<'v>>,
-    values: &LastValues,
-) -> Buffer<W, Body<'v>> {
-    buf.node("div")
-        .attr((
-            "class",
-            "card-content justify-content-center min-h-150px flex-col",
-        ))
-        .content(|buf| {
-            buf.node("div")
-                .attr(("class", "m-sm"))
-                .attr(("data-label", "moisture"))
-                .content(|buf| buf.raw(Formatter::si().with_unit("%").format(values.moisture)))
-                .node("div")
-                .attr(("class", "m-sm"))
-                .attr(("data-label", "temperature"))
-                .content(|buf| buf.raw(Formatter::si().with_unit("°C").format(values.temperature)))
-                .node("div")
-                .attr(("class", "m-sm"))
-                .attr(("data-label", "brightness"))
-                .content(|buf| buf.raw(Formatter::si().with_unit("lx").format(values.brightness)))
-                .node("div")
-                .attr(("class", "m-sm"))
-                .attr(("data-label", "conductivity"))
-                .content(|buf| {
-                    buf.raw(
-                        Formatter::si()
-                            .with_unit("μS/cm")
-                            .format(values.conductivity),
-                    )
-                })
-                .node("div")
-                .attr(("class", "m-sm"))
-                .attr(("data-label", "battery"))
-                .content(|buf| buf.raw(Formatter::si().with_unit("%").format(values.battery)))
-        })
-}
-
 #[derive(Debug)]
 pub struct LastValues {
     pub timestamp: u64,
@@ -68,6 +18,12 @@ pub struct MifloraCard {
     address: Cow<'static, str>,
     name: Option<Cow<'static, str>>,
     last_update: Option<LastValues>,
+
+    temperature_fmt: human_number::Formatter<'static>,
+    brightness_fmt: human_number::Formatter<'static>,
+    moisture_fmt: human_number::Formatter<'static>,
+    conductivity_fmt: human_number::Formatter<'static>,
+    battery_fmt: human_number::Formatter<'static>,
 }
 
 impl MifloraCard {
@@ -80,6 +36,54 @@ impl MifloraCard {
             address: address.into(),
             name: name.map(|n| n.into()),
             last_update,
+
+            temperature_fmt: Formatter::si().with_unit("°C"),
+            brightness_fmt: Formatter::si().with_unit("lx"),
+            moisture_fmt: Formatter::si().with_unit("%"),
+            conductivity_fmt: Formatter::si().with_unit("μS/cm"),
+            battery_fmt: Formatter::si().with_unit("%"),
+        }
+    }
+
+    fn render_last_update<'v, W: std::fmt::Write>(
+        &self,
+        buf: Buffer<W, Body<'v>>,
+    ) -> Buffer<W, Body<'v>> {
+        if let Some(ref values) = self.last_update {
+            buf.node("div")
+                .attr((
+                    "class",
+                    "card-content justify-content-center min-h-150px flex-col",
+                ))
+                .content(|buf| {
+                    buf.node("div")
+                        .attr(("class", "m-sm"))
+                        .attr(("data-label", "moisture"))
+                        .content(|buf| buf.raw(self.moisture_fmt.format(values.moisture)))
+                        .node("div")
+                        .attr(("class", "m-sm"))
+                        .attr(("data-label", "temperature"))
+                        .content(|buf| buf.raw(self.temperature_fmt.format(values.temperature)))
+                        .node("div")
+                        .attr(("class", "m-sm"))
+                        .attr(("data-label", "brightness"))
+                        .content(|buf| buf.raw(self.brightness_fmt.format(values.brightness)))
+                        .node("div")
+                        .attr(("class", "m-sm"))
+                        .attr(("data-label", "conductivity"))
+                        .content(|buf| buf.raw(self.conductivity_fmt.format(values.conductivity)))
+                        .node("div")
+                        .attr(("class", "m-sm"))
+                        .attr(("data-label", "battery"))
+                        .content(|buf| buf.raw(self.battery_fmt.format(values.battery)))
+                })
+        } else {
+            buf.node("div")
+                .attr((
+                    "class",
+                    "card-content align-content-center text-center min-h-150px",
+                ))
+                .content(|buf| buf.text("No content found"))
         }
     }
 }
@@ -89,10 +93,7 @@ impl super::prelude::Component for MifloraCard {
         buf.node("div")
             .attr(("class", "card miflora shadow min-w-250px m-md"))
             .content(|buf| {
-                let buf = match self.last_update {
-                    Some(ref values) => render_last_update(buf, values),
-                    None => render_empty_last_update(buf),
-                };
+                let buf = self.render_last_update(buf);
                 buf.node("div")
                     .attr(("class", "card-footer"))
                     .content(|buf| {
